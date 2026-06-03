7e3151791bb07b0c478549f5ccb5e552d75a5a2a
dennyD2 <93204748+dennyD2@users.noreply.github.com>
2026-06-04 02:40:34 +0530

    Update config_sagility.py

--- config_sagility.py
+++ config_sagility.py
@@ -2,6 +2,8 @@ COMPANY = "Sagility"
 
 MODE = "flow"
 
+FLOW_RUNNER = "monitor_sagility"
+
 FLOW_DOC_PATH = "Flow document.docx"
 
 BASE_URL = "https://hire-qa.bling-ai.com/sagility?reqId=REQ-017239&country='US'&location='TX'&source=SOURCE-3-125&profileID=IND007053"
