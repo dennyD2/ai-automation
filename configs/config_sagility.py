2decb76d7cc41260fffefcec9e37ba7e3d8dacfc
dennyD2 <93204748+dennyD2@users.noreply.github.com>
2026-06-11 02:29:39 +0530

    Add QA_VOICE and QA_NON_VOICE URLs to config

--- configs/config_sagility.py
+++ configs/config_sagility.py
@@ -1,8 +1,8 @@
 URLS = {
     "DEV": "https://hire-dev.bling-ai.com/sagility",
     "QA": {
-        "voice": "https://hire-qa.bling-ai.com/sagility?reqId=REQ-017239&country='US'&location='TX'&source=SOURCE-3-125&profileID=IND008188",
-        "non_voice": "https://hire-qa.bling-ai.com/sagility?reqId=REQ-017239&country='US'&location='TX'&source=SOURCE-3-125&profileID=IND007053"
+        "QA_VOICE": "https://hire-qa.bling-ai.com/sagility?reqId=REQ-017239&country='US'&location='TX'&source=SOURCE-3-125&profileID=IND008188",
+        "QA_NON_VOICE": "https://hire-qa.bling-ai.com/sagility?reqId=REQ-017239&country='US'&location='TX'&source=SOURCE-3-125&profileID=IND007053"
     },
     "PROD": "https://sagility.bling-ai.com/sagility"
 }
