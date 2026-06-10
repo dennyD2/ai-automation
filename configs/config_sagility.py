90fab04c4097939633da8afbb81172f9fc8247b1
dennyD2 <93204748+dennyD2@users.noreply.github.com>
2026-06-11 02:09:57 +0530

    Update config_sagility.py

--- configs/config_sagility.py
+++ configs/config_sagility.py
@@ -1,11 +1,8 @@
-COMPANY = "Sagility"
-
-MODE = "flow"
-
-FLOW_RUNNER = "monitor_sagility"
-
-FLOW_DOC_PATH = "Flow document.docx"
-
-BASE_URL = "https://hire-qa.bling-ai.com/sagility?reqId=REQ-017239&country='US'&location='TX'&source=SOURCE-3-125&profileID=IND007053"
-
-DISCORD_WEBHOOK = ""
+URLS = {
+    "DEV": "https://hire-dev.bling-ai.com/sagility",
+    "QA": {
+        "voice": "https://hire-qa.bling-ai.com/sagility?reqId=REQ-017239&country='US'&location='TX'&source=SOURCE-3-125&profileID=IND008188",
+        "non_voice": "https://hire-qa.bling-ai.com/sagility?reqId=REQ-017239&country='US'&location='TX'&source=SOURCE-3-125&profileID=IND007053"
+    },
+    "PROD": "https://sagility.bling-ai.com/sagility"
+}
